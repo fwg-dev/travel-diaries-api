5cf0d6ca7c26bdbe1c2d46ec60849eefbbaa34b0
fwg-dev <fwgicheh@gmail.com>
2020-10-31 01:41:54 -0700

    set params and methods

--- app/controllers/api/v1/locations_controller.rb
+++ app/controllers/api/v1/locations_controller.rb
@@ -1,2 +1,24 @@
 class Api::V1::LocationsController < ApplicationController
+
+  before_action :set_location, only: [:show, :update, :destroy]
+
+  # GET /attractions
+  def index
+    @locations = Location.all
+
+    render json: @locations
+  end
+  
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_location
+    @location = Location.find(params[:id])
+  end
+
+  # Only allow a trusted parameter "white list" through.
+  def location_params
+    params.require(:location).permit(:city, :region, :currency, :languages, :image_url)
+  end
+
+
 end
