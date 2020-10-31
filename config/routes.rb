80fe5a033e120096f7315856d7aaf6aea675bc1d
fwg-dev <fwgicheh@gmail.com>
2020-10-30 21:53:11 -0700

    wrap users resources with namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  resources :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #I want my api to be namespaced traveler.com/api/v1/resources
+  namespace :api do 
+    namespace :v1 do
+      resources :users
+    end
+  end
+   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
