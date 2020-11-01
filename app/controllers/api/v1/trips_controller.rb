59e46200cb8be2de8981186e33d5549ee52dfb5a
fwg-dev <fwgicheh@gmail.com>
2020-10-31 21:33:01 -0700

    add api/v1 to controller

--- app/controllers/api/v1/trips_controller.rb
+++ app/controllers/api/v1/trips_controller.rb
@@ -1,2 +1,25 @@
-class TripsController < ApplicationController
+class Api::V1::TripsController < ApplicationController
+
+  
+  before_action :set_location, only: [:show, :update, :destroy]
+
+  # GET /attractions
+  def index
+    @trips = Trip.all
+
+    render json: @trips
+  end
+  
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_location
+    @trip = Trip.find(params[:id])
+  end
+
+  # Only allow a trusted parameter "white list" through.
+  def location_params
+    params.require(:trip).permit(:date_of_trip)
+  end
+
+
 end
