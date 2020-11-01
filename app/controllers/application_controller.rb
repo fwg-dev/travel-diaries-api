cadebe99d7bc5c37184051cb9ba2c7f80c4c05ae
fwg-dev <fwgicheh@gmail.com>
2020-10-31 23:46:36 -0700

    add mock current user and logged_in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::API
+
+    def current_user 
+      #mock version 
+      User.first
+    end
+
+    def logged_in? 
+      !!current_user
+    end
+  
 end
