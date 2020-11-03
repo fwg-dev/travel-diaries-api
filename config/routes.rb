d52454afebe255c5e86debc64a41238f97482a60
fwg-dev <fwgicheh@gmail.com>
2020-11-03 08:30:02 -0800

    added sessions route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
- 
+post "/api/v1/login", to: "api/v1/sessions#create"
+
   #I want my api to be namespaced traveler.com/api/v1/resources
   namespace :api do 
     namespace :v1 do
@@ -7,6 +8,7 @@ Rails.application.routes.draw do
       resources :attractions
       resources :locations
       resources :users
+
     end
   end
    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
